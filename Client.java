b4a4f4a63e8db9de19d219418f8fa53f099ef55f
spydormunkay <cbutron1996@gmail.com>
2017-01-26 19:38:36 -0500

    Add files via upload
    Will update periodically to make it less... "cluttered"
    
    - Will add ability to scroll through chat box and message field.
    - Will add ability to whisper to other clients just because.

---
+++ Client.java
@@ -0,0 +1,159 @@
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+class Client {
+	
+	static boolean finished = false;
+	
+	public static class ChatBox extends JFrame implements Runnable {
+	    
+	    JPanel[] row = new JPanel[2];
+	    JButton sendButton = null;
+	    
+	    Dimension chatDimension = new Dimension(350, 160);
+	    Dimension textBoxDimension = new Dimension(200, 40);
+	    Dimension sendDimension = new Dimension(90, 40);
+
+	    JTextArea chat = new JTextArea(1,20);
+	    JTextField textBox = new JTextField(20);
+	    Font font = new Font("Times New Roman", Font.BOLD, 16);
+	    
+	    PrintWriter pout = null;
+	    BufferedReader bin = null;
+	    
+	    public ChatBox(Socket server) throws Exception {
+	        super("Chat Box Client");
+	        
+	        OutputStream out = server.getOutputStream();
+			pout = new PrintWriter(out, true);
+			InputStream in = server.getInputStream();
+			bin = new BufferedReader(new InputStreamReader(in));
+			
+	        //setSize(400, 400);
+	        setResizable(true);
+	        setDefaultCloseOperation(EXIT_ON_CLOSE);
+	        GridLayout grid = new GridLayout(2,2);
+	        setLayout(grid);
+	        
+	        FlowLayout f1 = new FlowLayout(FlowLayout.CENTER,10,10);
+	        FlowLayout f2 = new FlowLayout(FlowLayout.CENTER,10,10);
+	        for(int i = 0; i < 2; i++)
+	            row[i] = new JPanel();
+	        row[0].setLayout(f1);
+	        row[1].setLayout(f2);
+	        
+	        sendButton = new JButton();
+	        sendButton.setText("Send");
+	        sendButton.setFont(font);
+	        sendButton.addActionListener(new ActionListener() {
+	        	public void actionPerformed(ActionEvent ae) {
+	        		send();
+	        	}
+	        });
+	        
+	        textBox.addKeyListener(new KeyListener() {
+	        	public void keyPressed(KeyEvent e) {
+	        		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+	        			send();
+	        		}
+	        	}
+	        	public void keyReleased(KeyEvent e) { }
+	        	public void keyTyped(KeyEvent e) { }
+	        });
+	        
+	        chat.setFont(font);
+	        chat.setEditable(false);
+	        chat.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+	        chat.setPreferredSize(chatDimension);
+	        
+	        textBox.setFont(font);
+	        textBox.setEditable(true);
+	        textBox.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+	        textBox.setPreferredSize(textBoxDimension);
+	        
+	        sendButton.setPreferredSize(sendDimension);
+	        
+	        row[0].add(chat);
+	        add(row[0]);
+	        
+	        row[1].add(textBox);
+	        row[1].add(sendButton);
+	        add(row[1]);
+	        
+	        pack();
+	        setLocationRelativeTo(null);
+	        setVisible(true);
+	    }
+	    
+	    public void send() {
+	    	String request = textBox.getText();
+	    	if(request.equals("/quit")) {
+				Client.finished = true;
+				//chat.append("You are leaving the server.\n");
+			}
+			pout.println(request);
+	    	textBox.setText("");
+	    }
+	    
+	    public void send(String text) {
+	    	chat.append(text + "\n");
+	    }
+	    
+	    public void run() {
+	    	while(!Client.finished) {
+				try {
+					String response = bin.readLine();
+					send(response);
+				} catch(IOException e) {
+					if(!Client.finished) {
+						System.out.println(e.getMessage());
+					}
+				}
+			}
+		}
+	}
+  
+	public static void main(String[] args) throws Exception {
+		Socket server = null;
+		Scanner input = new Scanner(System.in);
+		try {
+			//ChatBox c = new ChatBox();
+			//c.send("What IP do you want to connect to?");
+			System.out.println("What IP do you want to connect to?");
+			String ip = input.nextLine();
+			server = new Socket(ip, 1337);
+			ChatBox c = new ChatBox(server);
+			Thread cThread = new Thread(c);
+			cThread.start();
+			cThread.join();
+    	} catch (IOException e ) {
+    		System.out.println(e.getMessage());
+    	} finally {
+    		if (server != null) {
+    			try {
+    				server.close();
+    			} catch (IOException e) {
+    				System.out.println(e.getMessage());
+    			}
+    		}
+    		input.close();
+    	}
+	}
+}
---
+++ Server.java
@@ -0,0 +1,228 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+class Server {
+	
+  private static ServerSocket serverSocket = null;
+  private static Socket clientSocket = null;
+
+  private static final int maxClientsCount = 10;
+  private static final clientThread[] threads = new clientThread[maxClientsCount];
+
+  public static void main(String args[]) throws Exception {
+	  
+    int portNumber = 1337;
+    
+    try {
+      serverSocket = new ServerSocket(portNumber);
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+    
+    ChatBox c = new ChatBox();
+    
+    //System.out.println("Server port number: " + portNumber);
+    c.send("Server port number: " + portNumber);
+    
+    while (true) {
+      try {
+        clientSocket = serverSocket.accept();
+        int i = 0;
+        for (i = 0; i < maxClientsCount; i++) {
+          if (threads[i] == null) {
+            (threads[i] = new clientThread(clientSocket, threads, c)).start();
+            break;
+          }
+        }
+        if (i == maxClientsCount) {
+          OutputStream out = clientSocket.getOutputStream();
+          PrintWriter pout = new PrintWriter(out, true);
+          pout.println("Server too busy. Try later.");
+          pout.close();
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println(e);
+      }
+    }
+  }
+}
+
+class clientThread extends Thread {
+
+  private String name = null;
+  private Socket clientSocket = null;
+  private final clientThread[] threads;
+  private int maxClientsCount;
+  
+  private BufferedReader bin = null;
+  private PrintWriter pout = null;
+  
+  private ChatBox c = null;
+
+  public clientThread(Socket clientSocket, clientThread[] threads, ChatBox x) throws Exception {
+    this.clientSocket = clientSocket;
+    this.threads = threads;
+    maxClientsCount = threads.length;
+    
+    InputStream in = clientSocket.getInputStream();
+	bin = new BufferedReader(new InputStreamReader(in));
+	  
+	OutputStream out = clientSocket.getOutputStream();
+	pout = new PrintWriter(out, true);
+	
+	c = x;
+  }
+
+  public void run() {
+    int maxClientsCount = this.maxClientsCount;
+    clientThread[] threads = this.threads;
+
+    try {
+      while (true) {
+        pout.println("Enter your name.");
+        name = bin.readLine();
+        break;
+      }
+      
+      pout.println("Welcome to the chatroom, " + name + ".");
+      pout.println("To leave enter /quit in a new line.");
+      synchronized (this) {
+        for (int i = 0; i < maxClientsCount; i++) {
+          if (threads[i] != null && threads[i] != this) {
+            threads[i].pout.println("*** " + name + " entered the chat room! ***");
+          }
+        }
+        //System.out.println("*** " + name + " entered the chat room! ***");
+        c.send("*** " + name + " entered the chat room! ***");
+      }
+      
+      while (true) {
+        String line = bin.readLine();
+        if (line.startsWith("/quit")) {
+          break;
+        }
+        synchronized (this) {
+          for (int i = 0; i < maxClientsCount; i++) {
+            if (threads[i] != null && threads[i].name != null) {
+              threads[i].pout.println("<" + name + "> " + line);
+            }
+          }
+          //System.out.println("<" + name + "> " + line);
+          c.send("<" + name + "> " + line);
+        }
+      }
+      synchronized (this) {
+        for (int i = 0; i < maxClientsCount; i++) {
+          if (threads[i] != null && threads[i] != this && threads[i].name != null) {
+            threads[i].pout.println("*** The user " + name + " is leaving the chat room. ***");
+          }
+        }
+        //System.out.println("*** The user " + name + " is leaving the chat room. ***");
+        c.send("*** The user " + name + " is leaving the chat room. ***");
+      }
+      pout.println("*** Bye " + name + " ***");
+      
+      synchronized (this) {
+        for (int i = 0; i < maxClientsCount; i++) {
+          if (threads[i] == this) {
+            threads[i] = null;
+          }
+        }
+      }
+      
+      bin.close();
+      pout.close();
+      clientSocket.close();
+    } catch (IOException e) {
+    }
+  }
+}
+
+class ChatBox extends JFrame {
+    
+    JPanel[] row = new JPanel[2];
+    JButton sendButton = null;
+    
+    Dimension chatDimension = new Dimension(350, 160);
+    Dimension textBoxDimension = new Dimension(200, 40);
+    Dimension sendDimension = new Dimension(90, 40);
+
+    JTextArea chat = new JTextArea(1,20);
+    JTextField textBox = new JTextField(20);
+    Font font = new Font("Times New Roman", Font.BOLD, 16);
+    
+    String name = null;
+    
+    ChatBox() {
+        super("Chat Box Server");
+        //setSize(400, 400);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        GridLayout grid = new GridLayout(2,2);
+        setLayout(grid);
+        
+        FlowLayout f1 = new FlowLayout(FlowLayout.CENTER,10,10);
+        FlowLayout f2 = new FlowLayout(FlowLayout.CENTER,10,10);
+        for(int i = 0; i < 2; i++)
+            row[i] = new JPanel();
+        row[0].setLayout(f1);
+        row[1].setLayout(f2);
+        
+        sendButton = new JButton();
+        sendButton.setText("Send");
+        sendButton.setFont(font);
+        sendButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent ae) {
+        		send();
+        	}
+        });
+        
+        textBox.addKeyListener(new KeyListener() {
+        	public void keyPressed(KeyEvent e) {
+        		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+        			send();
+        		}
+        	}
+        	public void keyReleased(KeyEvent e) { }
+        	public void keyTyped(KeyEvent e) { }
+        });
+        
+        chat.setFont(font);
+        chat.setEditable(false);
+        chat.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        chat.setPreferredSize(chatDimension);
+        
+        textBox.setFont(font);
+        textBox.setEditable(true);
+        textBox.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        textBox.setPreferredSize(textBoxDimension);
+        
+        sendButton.setPreferredSize(sendDimension);
+        
+        row[0].add(chat);
+        add(row[0]);
+        
+        row[1].add(textBox);
+        row[1].add(sendButton);
+        add(row[1]);
+        
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    
+    public void send() {
+    	chat.append(textBox.getText());
+    	textBox.setText("");
+    }
+    
+    public void send(String text) {
+    	chat.append(text + "\n");
+    }
+}
